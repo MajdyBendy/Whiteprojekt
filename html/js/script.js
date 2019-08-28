5d3f519d8fd0960105f5ee9e0dde5ff3f7064e71
HanelovaKa <54575329+HanelovaKa@users.noreply.github.com>
2019-08-28 20:47:59 +0200

    slideshow

--- html/js/script.js
+++ html/js/script.js
@@ -4,13 +4,17 @@ showSlides();
 function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
+
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    {
-        slides[slideIndex - 1].style.display = "block";
-        setTimeout(showSlides, 2000);
+    if (slideIndex >= slides.length) { 
+        slideIndex = 0;
     }
+
+    
+        slides[slideIndex].style.display = "block";
+        setTimeout(showSlides, 2000);
+    
 }
